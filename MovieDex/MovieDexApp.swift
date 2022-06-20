042e69065c82e60982c9ef728ffc6e13014cdb10
Dexston <mrdexston@gmail.com>
2022-06-20 12:47:36 +0300

    Initial Commit

---
+++ MovieDex/MovieDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDexApp.swift
+//  MovieDex
+//
+//  Created by Admin on 20.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
